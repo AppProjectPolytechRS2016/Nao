dfe20cf808cca4fd9fcd2e6f4c5b4e75140d56f4
sartorin <sartorin@i3-04-x64.ens-epua.univ-savoie.fr>
2015-03-27 12:06:30 +0100

    LAST UPDATE

--- AppFinale/NaoApplication.py
+++ AppFinale/NaoApplication.py
@@ -46,14 +46,16 @@ def main():
     'JSON Object Initialization '
     data_ident = {u'From':'193.48.125.67', u'To':'193.48.125.68', u'MsgType':'Ident', u'EquipmentType':'Robot'}
     
+    'Serializing Object Data to a JSON formated str'
+    result_ident = json.dumps(data_ident)
+    
+    print type(json.loads(result_ident)["MsgType"])
+    
     'New Socket Initialization'
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     
     'Connection to ComManager'
     s.connect(('193.48.125.68', 6030))
-    
-    'Serializing Object Data to a JSON formated str'
-    result_ident = json.dumps(data_ident)
     
     'Sending the Ident Message to the Server'
     s.send(result_ident+"\r\n")
@@ -61,15 +63,21 @@ def main():
     'Printing the Message sent'
     print ("Message sent to the server: \n"+result_ident)
     
+    'Receiving Message from the Server'
+    server_msg = s.recv(1024)
+    
+    'Main LOOP of the application'
+    while json.loads(json.dumps(s.recv(1024)))["MsgType"] != "End":
+        print ("Message received from the server: \n"+server_msg)
+        if json.loads(json.dumps(s.recv(1024)))["OrderName"] != "Init":
+            iR.runOnRobot(nao)
+    
     'Tests running different features on a specified robot'         
     #iR.runOnRobot(nao)
     #sR.runOnRobot(nao)
     #wA.runOnRobot(nao)
     #mR.runOnRobot(nao)
     #kR.runOnRobot(nao)  #!!!! WARNING NOT FUNCTIONAL
-    
-    'Receiving Message from the Server'
-    server_msg = s.recv(1024)
     
     'Printing the Message received'
     print ("Message received from the server: \n"+server_msg)
