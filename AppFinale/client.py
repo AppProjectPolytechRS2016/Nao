00c101afc9119433f32a316f7b3e8d8815397b7c
sartorin <sartorin@i3-04-x64.ens-epua.univ-savoie.fr>
2015-03-27 10:11:42 +0100

    Update Client connection

--- AppFinale/client.py
+++ AppFinale/client.py
@@ -1,6 +1,20 @@
 import socket
 import json
 
+# IP Jerome : '193.48.125.68' port: 6030
+
+data = {u'From':'193.48.125.67', u'To':'193.48.125.68', u'MsgType':'Ident', u'EquipmentType':'Robot'}
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
 s.connect(('193.48.125.68', 6030))
+result = json.dumps(data)
+lenght = len(result)
+
+s.send(result+"\r\n")
+
+print result
+result_into_json = json.loads(result)
+print result_into_json["From"]
+
 s.close()
